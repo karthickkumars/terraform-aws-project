52ec1ccd4ff64de4f611d64f6040e2b2186f2179
karthickkumars <109604756+karthickkumars@users.noreply.github.com>
2022-07-25 18:10:15 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,18 @@
 module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
+  source  = "git@github.com:karthickkumars/terraform-aws-modules//ec2?ref=master"
+  # version = "~> 3.0"
 
   name = "single-instance"
 
-  ami                    = "ami-ebd02392"
+  ami                    = "ami-05803413c51f242b7"
   instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
+  # key_name               = "user1"
+  # monitoring             = true
+  # vpc_security_group_ids = ["sg-12345678"]
+  # subnet_id              = "subnet-eddcdzz4"
 
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
+  # tags = {
+  # Terraform   = "true"
+  # Environment = "dev"
+  # }
 }
